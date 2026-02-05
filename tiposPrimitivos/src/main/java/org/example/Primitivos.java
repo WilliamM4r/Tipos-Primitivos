26fd739405576906600d6d8ef4b76c3421b706ed
William Marques <167483503+WilliamM4r@users.noreply.github.com>
2026-02-05 20:05:44 -0300

    Add files via upload

---
+++ tiposPrimitivos/src/main/java/org/example/Primitivos.java
@@ -0,0 +1,13 @@
+package org.example;
+public class Primitivos {
+    static void main() {
+        byte a = -128;
+        int b = 2357;
+        double c = 23.465;
+        boolean d = true;
+        char e = 'A';
+        float f = 23.4f;
+        long g = 85839467;
+        short h = 23887;
+    }
+}
