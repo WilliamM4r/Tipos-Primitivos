e8665d78e3d5b542df3be83a6e825e59445303c6
William <william.silva@gmail.com>
2026-02-05 21:02:24 -0300

    update

--- tiposPrimitivos/src/main/java/org/example/Primitivos.java
+++ tiposPrimitivos/src/main/java/org/example/Primitivos.java
@@ -1,13 +1,34 @@
 package org.example;
 public class Primitivos {
-    static void main() {
+    static void main(String[] args) {
+        imprimirTiposPrimitivos();
+    }
+
+    private static void imprimirTiposPrimitivos () {
+        //Inteiros
         byte a = -128;
-        int b = 2357;
-        double c = 23.465;
-        boolean d = true;
-        char e = 'A';
-        float f = 23.4f;
-        long g = 85839467;
-        short h = 23887;
+        short b = 23887;
+        int c = 2357;
+        long d = 85839467;
+
+        //Ponto Flutuante
+        float e = 23.4f;
+        double f = 23.465;
+
+        //Character
+        char g = 'A';
+
+        //Boleano
+        boolean h = true;
+
+        //Printer
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
     }
 }
